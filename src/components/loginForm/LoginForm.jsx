30aaef8e3167cd02f6590d9f4302812746f0be39
Nour Alchami <n.alchami@gmail.com>
2024-02-23 16:08:23 +0100

    using local storage for rememberMe checkbox

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,13 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { loginSuccess } from '../../Redux/reducers/loginReducer'
 import { rememberMe } from '../../Redux/reducers/userReducer'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function LoginForm() {
-    const [email, setEmail] = useState('bla')
-    const [password, setPassword] = useState('blabla')
     const dispatch = useDispatch()
     const isRemembered = useSelector((state) => state.user.isRemembered)
+    console.log(window.localStorage)
+
+    const localS = window.localStorage.getItem('ArgentBank')
+    console.log(localS)
+    const localS2 = JSON.parse(localS)
+    const savedEmail = localS2.userEmail
+    console.log(savedEmail)
+    const savedPassword = window.localStorage.getItem('userPassword')
 
     const toggleRememberMe = () => {
         dispatch(rememberMe())
@@ -15,25 +21,37 @@ export default function LoginForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        isRemembered
-            ? localStorage.setItem('userEmail', email)
-            : localStorage.removeItem('userEmail')
-        isRemembered
-            ? localStorage.setItem('userPassword', password)
-            : localStorage.removeItem('userPassword')
-        dispatch(loginSuccess())
+        const formData = new FormData(e.target)
+
+        const payload = JSON.stringify({
+            userEmail: formData.get('username'),
+            userPassword: formData.get('password'),
+        })
+        window.localStorage.setItem('ArgentBank', payload)
         console.log(window.localStorage)
+        // dispatch(loginSuccess())
+        // console.log(window.localStorage)
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <div className="input-wrapper">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" defaultValue={email} />
+                <input
+                    name="username"
+                    type="text"
+                    id="username"
+                    defaultValue={savedEmail}
+                />
             </div>
             <div className="input-wrapper">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" defaultValue={password} />
+                <input
+                    name="password"
+                    type="password"
+                    id="password"
+                    defaultValue={savedPassword}
+                />
             </div>
             <div className="input-remember">
                 <input
