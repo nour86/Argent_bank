4639a11a80bb2a85f992859f7c8aa4570a061aa8
Nour Alchami <n.alchami@gmail.com>
2024-02-24 16:31:54 +0100

    handling case when localStorage is empty

--- src/Redux/services/localStorageServices.jsx
+++ src/Redux/services/localStorageServices.jsx
@@ -1,7 +1,7 @@
 export const getDataFromLocalStorage = async () => {
     console.log('getfrom function')
     const response = await JSON.parse(window.localStorage.getItem('ArgentBank'))
-    const email = response.userEmail
-    const password = response.userPassword
+    const email = response?.userEmail
+    const password = response?.userPassword
     return { email, password }
 }
