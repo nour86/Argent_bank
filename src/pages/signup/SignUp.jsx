59e857ce22bbbd4f33a7183ebf6a1bfd18370d37
Nour Alchami <n.alchami@gmail.com>
2024-03-06 18:25:14 +0100

    formulaire SignUp done

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -4,15 +4,91 @@ import auth_service, { signUp } from '../../Redux/services/apiServices'
 
 export default function SignUp() {
     const dispatch = useDispatch()
-    const [error, setError] = useState({ firstName: '', lastName: '' })
+    const [formValues, setFormValues] = useState({
+        firstName: { value: '', error: '' },
+        lastName: { value: '', error: '' },
+        userName: { value: '', error: '' },
+        email: { value: '', error: '' },
+        password: { value: '', error: '' },
+    })
+    // prettier-ignore
+    const formErrors = {
+        firstName: { 
+            pattern: '\^[A-Z-a-z\\ \-]{3,}$',
+            message:'at least 3 letters'
+        },
+        lastName: { 
+            pattern: '\^\[A-Z-a-z\\ \-]{3,20}$',
+            message:'20 letters max'
+        },
+        userName: {
+            pattern: '\.*',
+            message:''
+        },
+        email: {
+            pattern: "^[\\w-\\.]+@([\\w\-]+\\.)+[\\w-]{2,4}$",
+            message:'please provide a valid email' 
+        },
+        password: {
+            pattern: '\\w{8,}$',
+            message:'at least 8 characters'
+        },
+    }
+
+    const submitEnabled =
+        formValues.firstName.value !== '' &&
+        formValues.lastName.value !== '' &&
+        formValues.email.value !== '' &&
+        formValues.password.value !== ''
+
+    const handleChange = (e) => {
+        const newValue = {
+            [e.target.name]: { value: e.target.value, error: '' },
+        }
+        setFormValues({
+            ...formValues,
+            ...newValue,
+        })
+    }
+
+    function validateField(fieldName) {
+        const valueToTest = formValues[fieldName].value
+        const regex = new RegExp(formErrors[fieldName].pattern, 'g')
+        const result = regex.test(valueToTest)
+        return result
+    }
+
+    function validateForm() {
+        let formisValid = true
+
+        const keys = Object.keys(formValues)
+        for (const key of keys) {
+            if (validateField(key) == false) {
+                setFormValues({
+                    ...formValues,
+                    [key]: {
+                        value: formValues[key].value,
+                        error: formErrors[key].message,
+                    },
+                })
+                formisValid = false
+            }
+        }
+        return formisValid
+    }
+
+    console.log(formValues)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        // validate fields
 
-        console.log(e.target.name)
-        const formData = new FormData(e.target)
-        dispatch(auth_service.signUp(formData))
+        if (validateForm() == false) {
+            console.log('form invalide')
+        } else console.log('form valide')
+
+        // console.log(e.target.firstName.value)
+        // const formData = new FormData(e.target)
+        // dispatch(auth_service.signUp(formData))
     }
 
     return (
@@ -26,41 +102,57 @@ export default function SignUp() {
                         <input
                             name="firstName"
                             type="text"
-                            pattern="\w{3,}"
-                            title="3 lettres minimum"
-                            required
+                            value={formValues.firstName.value}
+                            onChange={handleChange}
                         />
+                        <p>{formValues.firstName.error}</p>
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="lastName">Last name*</label>
-                        <input name="lastName" type="text" required />
+                        <input
+                            name="lastName"
+                            type="text"
+                            value={formValues.lastName.value}
+                            onChange={handleChange}
+                        />
+                        <p>{formValues.lastName.error}</p>
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="userName">User name</label>
-                        <input name="userName" type="text" required />
+                        <input
+                            name="userName"
+                            type="text"
+                            value={formValues.userName.value}
+                            onChange={handleChange}
+                        />
+                        <p>{formValues.userName.error}</p>
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="email">Email*</label>
                         <input
                             name="email"
                             type="email"
-                            pattern="^[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}$"
-                            title="veuillez entrer une adresse email valide"
-                            required
+                            value={formValues.email.value}
+                            onChange={handleChange}
                         />
+                        <p>{formValues.email.error}</p>
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password">Password*</label>
                         <input
                             name="password"
                             type="password"
-                            pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-                            title="Au moins 8 charactères dont une lettre et un chiffre"
-                            required
+                            value={formValues.password.value}
+                            onChange={handleChange}
                         />
+                        <p>{formValues.password.error}</p>
                     </div>
 
-                    <button className="sign-in-button" type="submit">
+                    <button
+                        className="sign-in-button"
+                        type="submit"
+                        disabled={!submitEnabled}
+                    >
                         Sign Up
                     </button>
                 </form>
